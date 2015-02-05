6fb325d08d82550c58cfe06026e3f7e245466292
Jeffrey Gu <jeffwgu@gmail.com>
2015-02-05 11:11:51 -0700

    finished guesser

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+x = rand(101)
 
 get '/' do
-  "Hello_world"
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index
+end
+
+def check_guess(guess)
+  if guess.to_i > x then "too high"
+  elsif guess.to_i == x then "good job"
+  else then "too low"
+  end
 end
